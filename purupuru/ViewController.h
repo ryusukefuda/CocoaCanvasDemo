d7f6538437cbf979e90d689be972e763bd1b9773
ryusukefuda <ryukai_f@yahoo.co.jp>
2014-03-13 15:08:49 +0900

    Initial Commit

---
+++ purupuru/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  purupuru
+//
+//  Created by RyusukeFuda on 2014/03/13.
+//  Copyright (c) 2014年 RyusukeFuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ purupuru/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  purupuru
+//
+//  Created by RyusukeFuda on 2014/03/13.
+//  Copyright (c) 2014年 RyusukeFuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
